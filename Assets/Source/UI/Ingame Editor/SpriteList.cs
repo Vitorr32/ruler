ef0c2ae736e66dcfcbcf22bdfba102fc6997e496
Vitor Rodrigues Costa <vitorc1206@gmail.com>
2020-06-14 22:50:05 -0300

    Added new event to sprite template, implemented a scrollview for the sprites of the game

--- Assets/Source/UI/Ingame Editor/SpriteList.cs
+++ Assets/Source/UI/Ingame Editor/SpriteList.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteList : MonoBehaviour
 {
+    public enum SpriteFilter
+    {
+        TEEN,
+        YOUNG,
+        MIDDLE_AGE,
+        MALE,
+        FEMALE
+    }
+
     List<Sprite> sprites;
-    // Start is called before the first frame update
-    void Start() {
+    public Transform spriteListAchor;
+    public GameObject spriteTemplatePrefab;
+
+    private void Awake() {
+        SpriteTemplate.onSpriteTemplateClicked += SpriteTemplateSelected;
+        ShowAllSprites();
+    }
+
+    private void ShowAllSprites() {
+        StoreController.instance.sprites.ForEach(sprite => {
+            GameObject spriteTemplate = Instantiate(spriteTemplatePrefab, spriteListAchor);
+
+            spriteTemplate.GetComponent<SpriteTemplate>().StartUpSpriteTemplate(sprite, sprite.name);
+        });
+    }
+
+    private void SpriteTemplateSelected(Sprite sprite, string filename) {
 
     }
 
-    // Update is called once per frame
-    void Update() {
-
-    }
 }
---
+++ Assets/Source/UI/Ingame Editor/SpriteTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SpriteTemplate : MonoBehaviour, IPointerClickHandler
+{
+    public delegate void OnClick(Sprite sprite, string filename);
+    public static event OnClick onSpriteTemplateClicked;
+
+    public string m_filename;
+    public Sprite m_sprite;
+
+    public Text uiText;
+    public Image image;
+
+    public void StartUpSpriteTemplate(Sprite sprite, string filename) {
+        m_filename = filename;
+        m_sprite = sprite;
+
+        uiText.text = filename;
+        image.sprite = sprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        onSpriteTemplateClicked?.Invoke(m_sprite, m_filename);
+    }
+}
